82a52d80982acfedc7cedced6ec7b8d07396c332
Reza Azari Aghouieh <126381470+raz0208@users.noreply.github.com>
2026-05-24 12:39:24 +0000

    Code Update: FastAPI - create first app

--- Basic_App/main.py
+++ Basic_App/main.py
@@ -3,3 +3,8 @@ from fastapi import FastAPI
 
 # Create main app by calling FastAPI class
 app = FastAPI()
+
+# Define a route for the root URL
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
