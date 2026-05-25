026c5d21f15bc8023d81f71f2fae6e066e080cee
Reza Azari Aghouieh <126381470+raz0208@users.noreply.github.com>
2026-05-25 12:01:16 +0000

    Code Update: Practice CRUD methods in HTML requests

--- Basic_App/main.py
+++ Basic_App/main.py
@@ -7,4 +7,19 @@ app = FastAPI()
 # Define a route for the root URL
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Message": "Wlcome to FastAPI!"}
+
+# Define a simple route using post method
+@app.post("/items/")
+def create_item(name: str, price: float):
+    return {"Name": name, "price": price}
+
+# Define a simple route using put method
+@app.put("/items/{item_id}")
+def update_item(item_id: int, name: str, price: float):
+    return {"item_id": item_id, "Name": name, "price": price}
+
+# Define a simple route using delete method
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return {"item_id": item_id, "Message": "Item deleted successfully!"}
