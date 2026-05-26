af73d6377e8dea404fc9f48562c9736bb2ce62c3
Reza Azari Aghouieh <126381470+raz0208@users.noreply.github.com>
2026-05-26 10:54:30 +0000

    Code Update: FastAPI - Handle dynamic data with path and query parameters

--- Basic_App/main.py
+++ Basic_App/main.py
@@ -1,25 +1,44 @@
-# Import required libraries
+## **Day one of FastAPI Practice**
+
+# # Import required libraries
+# from fastapi import FastAPI
+
+# # Create main app by calling FastAPI class
+# app = FastAPI()
+
+# # Define a route for the root URL
+# @app.get("/")
+# def read_root():
+#     return {"Message": "Wlcome to FastAPI!"}
+
+# # Define a simple route using post method
+# @app.post("/items/")
+# def create_item(name: str, price: float):
+#     return {"Name": name, "price": price}
+
+# # Define a simple route using put method
+# @app.put("/items/{item_id}")
+# def update_item(item_id: int, name: str, price: float):
+#     return {"item_id": item_id, "Name": name, "price": price}
+
+# # Define a simple route using delete method
+# @app.delete("/items/{item_id}")
+# def delete_item(item_id: int):
+#     return {"item_id": item_id, "Message": "Item deleted successfully!"}
+
+## **Day two of FastAPI Practice**
 from fastapi import FastAPI
 
-# Create main app by calling FastAPI class
 app = FastAPI()
 
-# Define a route for the root URL
-@app.get("/")
-def read_root():
-    return {"Message": "Wlcome to FastAPI!"}
+# Define a route for user path parameter
+@app.get("/users/{user_id}")
+def read_user(user_id: int):
+    return {"user_id": user_id}
 
-# Define a simple route using post method
-@app.post("/items/")
-def create_item(name: str, price: float):
-    return {"Name": name, "price": price}
+# Define a route for string path parameter
+@app.get("/items/{item_name}")
+def read_item(item_name: str):
+    return {"item_name": item_name}
 
-# Define a simple route using put method
-@app.put("/items/{item_id}")
-def update_item(item_id: int, name: str, price: float):
-    return {"item_id": item_id, "Name": name, "price": price}
-
-# Define a simple route using delete method
-@app.delete("/items/{item_id}")
-def delete_item(item_id: int):
-    return {"item_id": item_id, "Message": "Item deleted successfully!"}
+# Define a route for query parameters
