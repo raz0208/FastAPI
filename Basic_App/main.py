3878f8a8ba20b6ab634157b97c8243c79a9fcef6
Reza Azari Aghouieh <126381470+raz0208@users.noreply.github.com>
2026-05-27 11:48:39 +0000

    FastAPI Practice: Code Update - Handling request and response (Request and Response Bodies)

--- Basic_App/main.py
+++ Basic_App/main.py
@@ -26,32 +26,53 @@
 # def delete_item(item_id: int):
 #     return {"item_id": item_id, "Message": "Item deleted successfully!"}
 
-## **Day two of FastAPI Practice**
+# ## **Day two of FastAPI Practice**
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# # # Define a route for user path parameter
+# # @app.get("/users/{user_id}")
+# # def read_user(user_id: int):
+# #     return {"user_id": user_id}
+
+# # # Define a route for string path parameter
+# # @app.get("/items/{item_name}")
+# # def read_item(item_name: str):
+# #     return {"item_name": item_name}
+
+# # # Define a route for query parameters
+# # @app.get("/users/")
+# # def road_user(user_id: int, name: str):
+# #     return {"user_id": user_id, "name": name}
+
+# @app.get("/users/{user_id}/details")
+# def read_user_details(user_id: int, include_email: bool = False):
+#     # user_details = {"user_id": user_id, "name": "John Doe"}
+#     if include_email:
+#         return {"user_id": user_id, "email": "include_email"}
+#     else:
+#         return {"user_id": user_id, "email": "Email not included"}
+
+## **Day three of FastAPI Practice**
+#### Handle request and response body using Pydantic models
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# # Define a route for user path parameter
-# @app.get("/users/{user_id}")
-# def read_user(user_id: int):
-#     return {"user_id": user_id}
+# Define a Pydantic model format for item
+class User(BaseModel):
+    name: str
+    age: int
 
-# # Define a route for string path parameter
-# @app.get("/items/{item_name}")
-# def read_item(item_name: str):
-#     return {"item_name": item_name}
+# # Define a route to create a user
+# @app.post("/users/")
+# async def create_user(user: User):
+#     return {"sended user": user.name, "sended age": user.age}
 
-# # Define a route for query parameters
-# @app.get("/users/")
-# def road_user(user_id: int, name: str):
-#     return {"user_id": user_id, "name": name}
-
-@app.get("/users/{user_id}/details")
-def read_user_details(user_id: int, include_email: bool = False):
-    # user_details = {"user_id": user_id, "name": "John Doe"}
-    if include_email:
-        return {"user_id": user_id, "email": "include_email"}
-    else:
-        return {"user_id": user_id, "email": "Email not included"}
-
-## **Day three of FastAPI Practice**
+# Define a route to create a user for response body
+@app.post("/users/{user_id}", response_model=User)
+async def create_user(user_id: int):
+    # Example user data, in a real application you would typically save this to a database
+    return User(name="John Doe", age=30)
