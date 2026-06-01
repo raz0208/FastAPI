d2d4dadd6136c9e08155aeefa18b2ed60e310cde
Reza Azari Aghouieh <126381470+raz0208@users.noreply.github.com>
2026-06-01 12:55:22 +0000

    FastAPI_MySQL_App_Practice: Connect FastAPI to a SQL database {1}

--- FastAPI_MySQL/main.py
+++ FastAPI_MySQL/main.py
@@ -28,3 +28,18 @@ def get_users():
     except mysql.connector.Error as e:
         # return error if connection or query fails
         raise HTTPException(status_code=500, detail=str(e)) # Code 500: if there is an issue with the database connection or query execution
+    
+# Add users to the database
+@app.post("/users/") # define a POST endpoint at the path "/users/" to add a new user to the database
+def add_user_to_db(name: str, role: str, age: int):
+    try:
+        connect_to_db = get_db_connection() # establish connection to the database
+        cursor = connect_to_db.cursor(dictionary=True) # create a cursor to execute SQL queries, set dictionary=True for better readability
+        cursor.execute("INSERT INTO users (name, role, age) VALUES (%s, %s, %s)", (name, role, age)) # execute SQL query to add a new user to the 'users' table
+        connect_to_db.commit() # commit the changes to the database
+        cursor.close() # close the cursor after executing the query
+        connect_to_db.close() # close the database connection
+        return {"message": "User added successfully"}
+    except mysql.connector.Error as e:
+        # return error if connection or query fails
+        raise HTTPException(status_code=500, detail=str(e)) # Code 500: if there is an issue with the database connection or query execution
