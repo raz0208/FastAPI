55d20d60dc16a4e2f17940d4268efb9c2779aa16
Reza Azari Aghouieh <126381470+raz0208@users.noreply.github.com>
2026-06-03 14:06:27 +0000

    FastAPI_SQLite_App: Install, import and setting up configuration {2}

--- FastAPI_SQLite_App/main.py
+++ FastAPI_SQLite_App/main.py
@@ -2,9 +2,11 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 
 # Import FastAPI required libraries
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 # Create an instance of the FastAPI class to define the API application
 app = FastAPI()
@@ -29,3 +31,18 @@ class User(Base):
     name = Column(String, index = True) # Column for the user's name
     email = Column(String, Unique = True, index = True) # Column for the user's email, which must be unique
     age = Column(Integer, limit = from 0 to 120, nullable = True) # Column for the user's age, with a limit of 0 to 120
+
+# Create the actual database tables based on the defined models
+Base.metadata.create_all(bind = engine) # This will create the 'users' table in the SQLite database if it doesn't already exist
+
+# Define function to dependency to get a database session
+def get_db():
+    db = SessionLocal() # Create a new database session
+    try:
+        yield db # Yield the session to be used in API endpoints
+    finally:
+        db.close() # Ensure the database session is closed after use
+
+# Define API endpoint to create a new user
+@app.post("/users/", response_model = User) # This endpoint will accept POST requests to create a new user and return the created user as a response
+def create_user(user: User, db: Session = Depends(get_db))
