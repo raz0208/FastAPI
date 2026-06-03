81546fbf26b3a1dc568fb83535859d69cfc753ac
Reza Azari Aghouieh <126381470+raz0208@users.noreply.github.com>
2026-06-03 13:45:07 +0000

    FastAPI_SQLite_App: Install, import and setting up configuration

--- FastAPI_SQLite_App/main.py
+++ FastAPI_SQLite_App/main.py
@@ -17,15 +17,3 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}) 
 
 # Create a sessionmaker factory to create database sessions
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Create a base class for our models using the declarative system
-Base = declarative_base(): # Foundation for all our database models
-
-# Define a User model that represents the users table in the database
-class User(Base):
-    __tablename__ = "users" # Name of the table in the database
-
-    id = Column(Integer, primary_key = True, index = true, limit = 10) # Primary key column for the user ID
-    name = Column(String, index = True) # Column for the user's name
-    email = Column(String, Unique = True, index = True) # Column for the user's email, which must be unique
-    age = Column(Integer, limit = from 0 to 120, nullable = True) # Column for the user's age, with a limit of 0 to 120
